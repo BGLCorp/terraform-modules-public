5df68e84508c5fb9e39b1865eb31576511b4b197
jgournet <jgournet@bglcorp.com.au>
2020-06-03 14:00:45 +1000

    adding check on account id

--- _global/confirm_context.sh
+++ _global/confirm_context.sh
@@ -3,7 +3,12 @@
 CURRENT_CONTEXT=$(kubectl config current-context | tr -d '\n')
 
 if [[ "${CURRENT_CONTEXT}" = *${1} ]]; then
-    echo "Current context '${CURRENT_CONTEXT}' ends with the input '${1}'"
+    if [[ "${CURRENT_CONTEXT}" = *${2}* ]]; then
+        echo "Current context '${CURRENT_CONTEXT}' ends with the input '${1}' and has the right account id ${2}"
+    else
+        echo -e "\n\n==============================================================\n\nCurrent context '${CURRENT_CONTEXT}' does not contain the right account ID '${2}'\n\n!!!!!!!! CHANGE YOU KUBECTL CONTEXT BEFORE PROCEEDING !!!!!!!!\n\n==============================================================\n\n"
+        exit 1
+    fi
 else
     echo -e "\n\n==============================================================\n\nCurrent context '${CURRENT_CONTEXT}' does not end with the input '${1}'\n\n!!!!!!!! CHANGE YOU KUBECTL CONTEXT BEFORE PROCEEDING !!!!!!!!\n\n==============================================================\n\n"
     exit 1
