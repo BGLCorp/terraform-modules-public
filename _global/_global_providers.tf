d51855ad1381936b11caed87ac35a57bfe1eb82d
jgournet <jgournet@bglcorp.com.au>
2021-01-18 14:07:30 +1100

    Revert "Move aws version to the right block"
    This reverts commit 9107beaf5fd318f07da9e20affdabb7535476704.

--- _global/_global_providers.tf
+++ _global/_global_providers.tf
@@ -5,18 +5,10 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
+  version = ">= 2.0.0"
   
   allowed_account_ids = var.aws_allowed_account_ids
 
   # Make it faster by skipping something
   skip_get_ec2_platforms = true
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.0.0"
-    }
-  }
 }
