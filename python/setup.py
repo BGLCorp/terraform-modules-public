62c9a4b59119652af574de966ae132602171c082
Eli Palchan <epalchan@bglcorp.com.au>
2026-03-30 11:26:52 +1100

    Fix new cloudevents version

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='BGLshared',
     packages=['BGLshared'],
-    version='0.0.7',
+    version='0.0.8',
     description='BGL shared functions and configuration.',
     install_requires=['python-json-logger'],
     extras_require={
